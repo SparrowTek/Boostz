caedbb450d13005145dbe5330f57d5eff14ab39f
Thomas Rademaker <thomas@sparrowtek.com>
2023-12-10 07:03:58 -0500

    setup as open source project

---
+++ Boostz/Modules/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Boostz
+//
+//  Created by Thomas Rademaker on 12/9/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Boostz/System/BoostzApp.swift
@@ -0,0 +1,17 @@
+//
+//  BoostzApp.swift
+//  Boostz
+//
+//  Created by Thomas Rademaker on 12/9/23.
+//
+
+import SwiftUI
+
+@main
+struct BoostzApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
