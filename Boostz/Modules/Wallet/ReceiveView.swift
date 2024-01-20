dca8fdef19bd5bf3f746016eeb1de4d31b484aad
Thomas Rademaker <thomas@sparrowtek.com>
2024-01-20 07:18:49 -0500

    receive view needed a done button

--- Boostz/Modules/Wallet/ReceiveView.swift
+++ Boostz/Modules/Wallet/ReceiveView.swift
@@ -8,8 +8,16 @@
 import SwiftUI
 
 struct ReceiveView: View {
+    @Environment(\.dismiss) private var dismiss
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack {
+            Text("Receive")
+                .toolbar {
+                    ToolbarItem(placement: .topBarTrailing) {
+                        Button("Done", action: { dismiss() })
+                    }
+                }
+        }
     }
 }
 
