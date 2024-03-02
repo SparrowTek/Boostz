29c0ab2ed03b75f3124bfb08ddf1c41ed90251fd
Thomas Rademaker <thomas@sparrowtek.com>
2024-03-02 07:53:59 -0500

    support checking an invoice status

--- Boostz/Modules/Wallet/Receive/DisplayInvoiceView.swift
+++ Boostz/Modules/Wallet/Receive/DisplayInvoiceView.swift
@@ -11,6 +11,13 @@ import AlbyKit
 struct DisplayInvoiceView: View {
     var invoice: CreatedInvoice
     @State private var invoiceCopied = false
+    @State private var checkInvoiceTrigger = PlainTaskTrigger()
+    @State private var invoiceResponse: Invoice?
+    @Environment(AlbyKit.self) private var alby
+    
+    private var isSettled: Bool {
+        invoiceResponse != nil && invoiceResponse?.state == .settled
+    }
     
     var body: some View {
         VStack {
@@ -19,9 +26,11 @@ struct DisplayInvoiceView: View {
                 .bold()
                 .padding()
             
-            Text("waiting for payment...")
-            Button("check status", action: checkInvoiceStatus)
+            Text(isSettled ? "Paid" : "waiting for payment...")
+            Button("check status", action: triggerCheckInvoiceStatus)
                 .buttonStyle(.bordered)
+                .disabled(isSettled)
+                .opacity(isSettled ? 0 : 1)
             
             ZStack {
                 Button(action: copyInvoice) {
@@ -40,10 +49,16 @@ struct DisplayInvoiceView: View {
             .padding(.vertical)
         }
         .padding()
+        .task($checkInvoiceTrigger) { await checkInvoiceStatus() }
     }
     
-    private func checkInvoiceStatus() {
-        // TODO: implement checkInvoiceStatus
+    private func triggerCheckInvoiceStatus() {
+        checkInvoiceTrigger.trigger()
+    }
+    
+    private func checkInvoiceStatus() async {
+        guard let invoice = try? await alby.invoicesService.getInvoice(withHash: invoice.paymentHash) else { return }
+        invoiceResponse = invoice
     }
     
     private func copyInvoice() {
@@ -59,4 +74,5 @@ struct DisplayInvoiceView: View {
 
 #Preview {
     DisplayInvoiceView(invoice: .init(amount: 4536, expiresAt: .now, paymentHash: "wwowow", paymentRequest: "lnbc555550n1pjal89dpp5alsdh4aewkpkmw7qf7jpv7g9ege6rac0na85g49403sa0naayjuqdp8fd2kgmtd248rwjmkd44yz6n5vaqnynt0x3jxwaccqzzsxqyz5vqsp5wug9d25x60l7ewslkmzuvlqvmq8mfvu8kzkhugyfuf9j34fmu4js9qyyssq4jyqjfhyyw9tt090w523rqjy08u9fqhry4ze34v8af3gvud6wy9h5nfayg0v8s2fynwtrsa0pcdzx582yssra97rt0kaqmu4uzp6zxgqjhlhlw"))
+        .environment(AlbyKit())
 }
