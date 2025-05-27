087f6fd7d148c8164ded0ff14149e65e606f95d0
Thomas Rademaker <thomas@radmakr.com>
2025-05-27 12:44:03 -0400

    fix logic for displaying dollar amount

--- Avocadough/Extensions/String.swift
+++ Avocadough/Extensions/String.swift
@@ -9,8 +9,17 @@ import Foundation
 
 extension String {
     
-    func asDollars(for sats: UInt64) -> String? {
-        guard let amount = Double(self) else { return nil }
-        return "$\(amount)" // TODO: THIS IS WRONG
+    func asDollars(for sats: UInt64) -> String? {        
+        guard let btcPrice = Double(self) else { return nil }
+        let btcAmount = Double(sats) / 100_000_000
+        let usdValue = btcPrice * btcAmount
+        
+        let formatter = NumberFormatter()
+        formatter.numberStyle = .currency
+        formatter.currencyCode = "USD"
+        formatter.minimumFractionDigits = 2
+        formatter.maximumFractionDigits = 2
+        
+        return formatter.string(from: NSNumber(value: usdValue))
     }
 }
